2ffbb25fa9c566553c8c31791357dc3bc755c02b
mahiiram <mahendran.ramar9797@gmail.com>
2024-03-14 01:52:24 +0530

    new commit

--- src/components/Navtoemail.js
+++ src/components/Navtoemail.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Navtoemail() {
   return (
     <div >
-      <Link to={'/signup'} style={{textDecoration:"none"}}><span style={{color:"black",fontSize:"20px",fontWeight:"500"}}>Signup with Phone Or Email</span></Link>
+      <Link to={'/signup'} style={{textDecoration:"none"}}><span style={{color:"black"}}>Signup with Phone Or Email</span></Link>
     </div>
   )
 }
